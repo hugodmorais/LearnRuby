87aacc77314b7909b6e3366a0bfead2205c80d42
hugodmorais <hugodanielmorais@gmail.com>
2020-06-24 12:00:03 +0100

    between method, #arguments

--- LearnToCodeWithRuby/basic.rb
+++ LearnToCodeWithRuby/basic.rb
@@ -6,3 +6,13 @@
 # par ou impar(par)
 1.even? # false
 2.even? # true
+
+# Arguments
+.between?(MIN, MAX) # MIN and MAX are arguments
+
+# between
+3.between?(1, 5) # true
+6.between?(1, 5) # false
+'cat'.between?('ant', 'dog') # true
+
+
