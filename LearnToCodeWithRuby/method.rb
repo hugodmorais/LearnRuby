1cac72750df031e27c94764e269769cd74749d7f
hugodmorais <hugodanielmorais@gmail.com>
2020-06-28 00:05:58 +0100

    method continue

--- LearnToCodeWithRuby/method.rb
+++ LearnToCodeWithRuby/method.rb
@@ -8,4 +8,20 @@ class Method
   def self.praise_person(name)
     puts "Olá #{name}."
   end
+
+  def self.add_two_numbers(num1, num2)
+    puts "OK, I'm solving your math problem!!"
+    total = num1 + num2
+    return  "Just kidding, I'm a troll!" if total > 20
+
+    total
+  end
+
+  def self.return_string
+    "What will be the return value here?"
+  end
+
+  def self.return_guess
+
+  end
 end
