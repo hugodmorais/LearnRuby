2fd6a00b31b29036581642291ad06ebb02f763fb
hugodmorais <hugodanielmorais@gmail.com>
2020-06-30 23:24:52 +0100

    case when then

--- LearnToCodeWithRuby/method.rb
+++ LearnToCodeWithRuby/method.rb
@@ -39,4 +39,14 @@ class Method
       "I don't know about that food!"
     end
   end
+
+  def self.calculate_school_grade(grade)
+    case grade
+    when 90..100 then "A"
+    when 80..89 then "B"
+    when 70..79 then "C"
+    when 60..69 then "D"
+    else "F"
+    end
+  end
 end
