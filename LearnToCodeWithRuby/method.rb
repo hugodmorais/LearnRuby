9e219da32a7a289944e7e495528df97a7f6d63c0
hugodmorais <hugodanielmorais@gmail.com>
2020-06-30 23:16:54 +0100

    case when

--- LearnToCodeWithRuby/method.rb
+++ LearnToCodeWithRuby/method.rb
@@ -24,4 +24,19 @@ class Method
   def self.return_guess
 
   end
+
+  def self.rate_my_food(food)
+    case food
+    when "Steak"
+      "Pass the steak sauce! That's delicious!"
+    when "Sushi"
+      "Greate choice! My favorite food!"
+    when "Tacos", "Burritos", "Quesadillas"
+      "Cheesy and filling! The perfect combination"
+    when "Tofu", "Brussel Sprouts"
+      "Disgusting! Yuck!"
+    else
+      "I don't know about that food!"
+    end
+  end
 end
