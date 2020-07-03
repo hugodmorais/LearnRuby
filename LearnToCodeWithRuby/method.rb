bbf1fe03ccbf3ec62c773cd1e118e85af109d12e
hugodmorais <hugodanielmorais@gmail.com>
2020-07-03 22:53:25 +0100

    include? while

--- LearnToCodeWithRuby/method.rb
+++ LearnToCodeWithRuby/method.rb
@@ -49,4 +49,36 @@ class Method
     else "F"
     end
   end
+
+  password = "dominoes"
+
+  if password.include?("a")
+    puts "It includes the letter a"
+  end
+
+  i = 1
+
+  while i < 10
+    puts i
+    i += 1
+  end
+
+  status = true
+
+  while status
+    print "Please enter username: "
+    username = gets.chomp.downcase
+    print "Please enter password"
+    password = gets.chomp.downcase
+
+    if username == "hugo" && password == "password"
+      puts "Entry granted"
+      status = false
+    elsif username =="quit" || password == "quit"
+      puts "Goodbye! Better luck next time!"
+      status = false
+    else
+      puts "Incorrect combination, try again!"
+    end
+  end
 end
