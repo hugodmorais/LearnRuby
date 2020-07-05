778ada7726e97acf6d63760b0fdb5a3b2b1dc28e
hugodmorais <hugodanielmorais@gmail.com>
2020-07-05 19:19:15 +0100

    Ranges, continue

--- LearnToCodeWithRuby/range.rb
+++ LearnToCodeWithRuby/range.rb
@@ -13,3 +13,19 @@ p nums.last(4) # [146, 147, 148, 149]
 
 nums = 90..150
 p nums.last(4) # [147, 148, 149, 150]
+
+alphabet = "a".."z"
+puts alphabet.last(1) # z
+
+alphabet = "A".."Z"
+puts alphabet.last(1) # Z
+
+numbers = 143..769
+p numbers.size # 625
+
+half_alphabet = "a".."m"
+puts half_alphabet.include?("j") # true
+
+p half_alphabet === "e" # true
+p half_alphabet === "z" # false
+
