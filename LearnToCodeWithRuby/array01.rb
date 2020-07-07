2d297a3aff11dd3661fbbc183b574fb590b63fd1
hugodmorais <hugodanielmorais@gmail.com>
2020-07-07 23:25:08 +0100

    The first and last methods

--- LearnToCodeWithRuby/array01.rb
+++ LearnToCodeWithRuby/array01.rb
@@ -37,6 +37,32 @@ p numbers.slice(3) # 3
 p numbers.slice(2, 3) # [2, 3, 4]
 p numbers.slice(4..6) # [4, 5, 6]
 
+puts [1, 2, 3].empty? # false
+puts [].empty? # true
+puts [].length == 0 # true
+
+arr = [1,3,5,7,9,15,21,18,6]
+p arr.first # 1
+p arr.last # 6
+p arr.first(3) # [1,3,5]
+p arr.last(2) # [18,6]
+
+def custom_first(arr, num = 0)
+  return arr[0] if num == 0
+
+  arr[0, num]
+end
+
+p custom_first(arr, 5)
+
+def custom_last(arr, num = 0)
+  return arr[-1] if num == 0
+
+  arr[-num..-1]
+end
+
+p custom_last(arr)
+
 
 
 
