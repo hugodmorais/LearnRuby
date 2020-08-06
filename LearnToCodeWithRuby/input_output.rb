318566955516eaf59fd0696cce711258f0b07e3d
hugodmorais <hugodanielmorais@gmail.com>
2020-08-06 22:19:24 +0000

    input output

--- LearnToCodeWithRuby/input_output.rb
+++ LearnToCodeWithRuby/input_output.rb
@@ -7,3 +7,18 @@ File.open("myFirstFile.txt", "a") do |file|
   file.print "No Line break here!"
   file.puts "Pretty cool!"
 end
+
+# ------------------------------------------- #
+File.rename("myFirstFile.txt", "somethingBetter.txt")
+
+File.delete("somethingBetter.txt")
+
+if File.exist?("myFirstFile.txt")
+  File.delete("myFirstFile.txt")
+end
+
+
+ARGV.each do |argument|
+  number = argument.to_i
+  puts "The square of #{number} is #{number ** 2}"
+end
