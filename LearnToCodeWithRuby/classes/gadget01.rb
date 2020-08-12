a458b929ee00ff28f642d391366232df98c334f9
hugodmorais <hugodanielmorais@gmail.com>
2020-08-12 22:17:52 +0000

    Class methods

--- LearnToCodeWithRuby/classes/gadget01.rb
+++ LearnToCodeWithRuby/classes/gadget01.rb
@@ -4,10 +4,19 @@ class Gadget
     @password = "topsecret"
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
+
+  def info
+    "Gadget #{@production_number} has the username #{@username}."
+  end
 end
+
+phone = Gadget.new
+puts phone.info # Gadget y-419 has the username User 33.
+
 
 phone = Gadget.new
 puts phone.inspect
 p phone
 p phone.instance_variables # [:@username, :@password, :@production_number]
 laptop = Gadget.new
+p laptop.instance_variables # [:@username, :@password, :@production_number]
