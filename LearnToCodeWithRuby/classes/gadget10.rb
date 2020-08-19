b0d95e3be29a7b2fb7d70c6d440301afa9ee48ae
hugodmorais <hugodanielmorais@gmail.com>
2020-08-19 22:28:44 +0000

    Class variables and Classes Methods

--- LearnToCodeWithRuby/classes/gadget10.rb
+++ LearnToCodeWithRuby/classes/gadget10.rb
@@ -5,6 +5,30 @@ class Bicycle
   def self.description
     "Hi ther, I´m the blueprint for bicycles!"
   end
+
+  def self.count
+    @@count
+  end
+
+  def maker
+    @@maker
+  end
+
+  def initialize
+    @@count += 1
+  end
 end
 
 puts Bicycle.description # "Hi ther, I´m the blueprint for bicycles!"
+p Bicycle.count # 0
+
+a = Bicycle.new
+a.maker # "BikeTech"
+
+# --------------------------------------------------------------------- #
+
+a = Bicycle.new
+b = Bicycle.new
+c = Bicycle.new
+
+p Bicycle.count # 3
