85f7a06598f635706248a9a96bce37116993e3f8
hugodmorais <hugodanielmorais@gmail.com>
2020-08-20 23:02:12 +0000

    Super and Subclasses

--- LearnToCodeWithRuby/classes/classes01/example.rb
+++ LearnToCodeWithRuby/classes/classes01/example.rb
@@ -16,15 +16,39 @@ hugo = Employee.new("Hugo", 35)
 puts hugo.introduce # "Hi, my name is Hugo and I am 35 years old"
 
 class Manager < Employee
+  def yell
+    "Who's the boss!!"
+  end
 
+  def introduce
+    "My name is #{name}"
+  end
 end
 
 class Worker < Employee
-
+  def clock_in(time)
+    "Starting my shift at #{time}"
+  end
 end
+
+p Manager.ancestors # [Manager, Employee, Object, Kernel, BasicObject]
 
 bob = Manager.new("Bob", 48)
 daniel = Worker.new("Daniel", 35)
 
-p bob # "Hi, my name is Bob and I am 48 years old"
-p daniel # "Hi, my name is Daniel and I am 35 years old"
+p bob.introduce # "Hi, my name is Bob and I am 48 years old"
+p daniel.introduce # "Hi, my name is Daniel and I am 35 years old"
+
+puts Manager.superclass # [Manager, Employee, Object, Kernel, BasicObject]
+
+puts bob.is_a?(Manager) # true
+puts daniel.is_a?(Manager) # false
+puts bob.instance_of?(Manager) # true
+
+p bob.yell # "Who's the boss!!"
+p dan.yell # error error error
+
+# -------------------------------------------------------------------------------- #
+
+sally = Manager.new("Sally", 45)
+p sally.introduce # "My name is Hugo"
